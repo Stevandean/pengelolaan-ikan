387d7ff027865e1a3651d962529a8347564f139e
Stevandeann <stevandeanachmad1@gmail.com>
2022-06-20 07:29:00 +0700

    add print pengeluaran

--- assets/js/laporan.js
+++ assets/js/laporan.js
@@ -3,12 +3,14 @@ function loadPembelianIkan() {
     let query = `select *, harga_pembelian / qty_pembelian as nominal_pembelian from pembelianIkan`
     db.serialize ( () => {
         db.all(query, (err, rows) => {
+            let totalPembelian = 0
             if (err) throw err
             let tr = ''
             if(rows.length <1) {
                 tr += ''
             } else {
                 rows.forEach((row) => {
+                    totalPembelian += row.nominal_pembelian
                     tr+=`<tr>
                             <td>${row.pembelian}</td>
                             <td>${row.tanggal_pembelian}</td>
@@ -17,6 +19,16 @@ function loadPembelianIkan() {
                             <td>${row.nominal_pembelian}</td>
                         </tr>`
                 })
+                tr += `
+                <tr>
+                    <td colspan="4">
+                        Jumlah
+                    </td>
+                    <td>
+                        Rp ${totalPembelian}
+                    </td> 
+                </tr>`
+
             }
             $('tbody#data-pembelian').html(tr)
             
@@ -29,12 +41,14 @@ function loadPenjualan() {
     let query = `select *, harga_penjualan * Qty_penjualan as jumlah_penjualan from penjualanIkan`
     db.serialize ( () => {
         db.all(query, (err, rows) => {
+            let totalPenjualan = 0
             if (err) throw err
             let tr = ''
             if(rows.length <1) {
                 tr += ''
             } else {
                 rows.forEach((row) => {
+                    totalPenjualan += row.jumlah_penjualan
                     tr+=`<tr>
                             <td>${row.tanggal_penjualan}</td>
                             <td>${row.harga_penjualan}</td>
@@ -43,7 +57,16 @@ function loadPenjualan() {
                             <td>${row.penjual_penjualan}</td>
                         </tr>`
                 })
-                tr+=`<td colspan = "6">Jumlah</td>       `
+                tr += `
+                <tr>
+                    <td colspan="4">
+                        Jumlah
+                    </td>
+                    <td>
+                        Rp ${totalPenjualan}
+                    </td> 
+                </tr>`
+
             }
             $('tbody#data-penjualan').html(tr)
         })
@@ -55,12 +78,14 @@ function loadPengeluaran() {
     let query = `select *, harga * qty as jumlah from pengeluaran`
     db.serialize ( () => {
         db.all(query, (err, rows) => {
+            let totalPengeluaran = 0
             if (err) throw err
             let tr = '' 
             if(rows.length <1) {
                 tr += ''
             } else {
                 rows.forEach((row) => {
+                    totalPengeluaran += row.jumlah
                     tr+=`<tr>
                             <td>${row.keterangan}</td>
                             <td>${row.tanggal}</td>
@@ -69,6 +94,16 @@ function loadPengeluaran() {
                             <td>${row.jumlah}</td>
                         </tr>`
                 })
+                tr += `
+                <tr>
+                    <td colspan="4">
+                        Jumlah
+                    </td>
+                    <td>
+                        Rp ${totalPengeluaran}
+                    </td> 
+                </tr>`
+
             }
             $('tbody#data-pengeluaran').html(tr)
         })
